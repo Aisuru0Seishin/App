4633073692a243e83092318f055b3eb98806fd55
Aisuru0Seishin <zoecyberpop344@gmail.com>
2024-05-12 20:12:04 -0600

    adding

---
+++ lib/pagess/main2.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/pagess/home_page.dart';
+import 'package:flutter_app/pagess/intro_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const IntroPage(),
+      routes: {
+        '/homepage': (context) => const HomePage(),
+        'intropage': (context) => const IntroPage(),
+      },
+    );
+  }
+}
